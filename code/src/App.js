4f5f1e59eb05a478d260e8b250c529dd317c2db3
Andrea Osmar <johndoe@email.com>
2020-10-22 17:09:46 +0200

    Updating

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,51 @@
-import React from 'react'
-import data from './data.json'
+import React from 'react';
+import data from '../data.json';
+import stretchGoal from '../stretch-goal.json';
+import { Album } from './Album';
+import { Heading } from './Heading';
+import { Playlist } from './Playlist';
+import './App.css';
 
-console.log(data)
+//const albumArray = albums.items;
+const albumArray = data.albums.items;
+
+//creates a new array of playlist in stretch goal json
+const playlistArray = stretchGoal.playlists.items;
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+	return (
+		<>
+			<header className="heading">
+				<Heading />
+			</header>
+
+			<section className="content-wrapper">
+				<div className="albums-container">
+					{albumArray.map(album => {
+						return (
+							<Album
+								key={album.id}
+								name={album.name}
+								image={album.images[1].url}
+								albumUrl={album.external_urls.spotify}
+								artists={album.artists}
+							/>
+						);
+					})}
+				</div>
+				<div className="playlist-container">
+					{playlistArray.map(playlist => {
+						return (
+							<Playlist
+								key={playlist.id}
+								image={playlist.images[0].url}
+								playlistUrl={playlist.external_urls.spotify}
+								name={playlist.name}
+							/>
+						);
+					})}
+				</div>
+			</section>
+		</>
+	);
+};
